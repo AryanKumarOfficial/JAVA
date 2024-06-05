a5df8b255f5f773b5cc3d3ce6a293bfdd506d696
Aryan Kumar <arynak9163@gmail.com>
2024-06-05 11:23:09 +0530

    relational operators

---
+++ relational_operators.java
@@ -0,0 +1,29 @@
+class relational_Operators {
+    public static void main(String args[]) {
+        System.out.println("Relational Operators");
+
+        // initailising varaibles
+
+        int x = 5;
+        int y = 6;
+        int z = 5;
+
+        // comparing variables
+
+        boolean result = x < y; // smaller than
+        boolean result1 = x > y; // greater than
+        boolean result2 = x == z; // equal to
+        boolean result3 = x >= y; // greater than or equel to
+        boolean result4 = x <= z; // smaller than or equel to
+        boolean result5 = x != y; // not equel to
+
+        // printing results
+
+        System.out.println(result);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
+    }
+}
