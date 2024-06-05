98db3b258f2006a035991be9dbd65513ecbc4a20
Aryan Kumar <arynak9163@gmail.com>
2024-06-05 11:50:19 +0530

    logical operations

---
+++ logicalOperators.java
@@ -0,0 +1,28 @@
+/**
+ * logicalOperators
+ */
+class logicalOperators {
+
+    public static void main(String[] args) {
+        System.out.println("Logical Operators");
+
+        // used to combine expressions
+
+        // AND - both true results true , false if any is false
+        // OR - any one is true gets true
+        // NOT - reverse the output, from false to true or true to false
+
+        int a = 5;
+        int b = 6;
+        int x = 10;
+        int y = 9;
+
+        boolean result;
+        result = a < b && x < y;
+        System.out.println(result);
+        result = a < b || x > y;
+        System.err.println(result);
+        result = !result;
+        System.err.println(result);
+    }
+}
