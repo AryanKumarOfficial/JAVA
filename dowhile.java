ecad6d5494158ca4bcb1cae0f8f74ba30fdd13ea
Aryan Kumar <arynak9163@gmail.com>
2024-06-07 15:33:04 +0530

    do while loop

---
+++ dowhile.java
@@ -0,0 +1,16 @@
+/**
+ * dowhile
+ */
+public class dowhile {
+
+    public static void main(String[] args) {
+        System.out.println("Do while loop\n");
+        // executes then check for the condition
+
+        int i = 1;
+        do {
+            System.out.println("Hello");
+            i++;
+        } while (i <= 5);
+    }
+}
