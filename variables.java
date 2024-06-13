5e9ddd4d4115ccae9d9508001073721cd4719cc6
Aryan Kumar <arynak9163@gmail.com>
2024-06-13 15:59:50 +0530

    learn about variables storing data in memory

---
+++ variables.java
@@ -0,0 +1,32 @@
+
+//  every method will have its own stack in memory for varaibles
+/*
+ * while we create a object it is stored in the heap memory and the instance variable is stored in the heap
+ * the address to the object will be stored in the variable for this example we have calc which stores the address of the object not the object itself
+ */
+
+class Calculator {
+
+    int x = 7; // instance variable
+
+    public int add(int a, int b) {
+
+        // a & b are local variable
+        // System.out.println(x);
+        return a + b;
+
+    }
+}
+
+public class variables {
+    public static void main(String[] args) {
+
+        Calculator calc = new Calculator();
+        Calculator calc2 = new Calculator();
+        calc.x = 10;
+        // int r = calc.add(4, 5);
+        // System.out.println(r);
+        System.out.println(calc.x);
+        System.out.println(calc2.x);
+    }
+}
