338860a8759044d6fd297ad71ef6b56970b7c04e
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-06 21:58:20 +0530

    static block in class

--- statics.java
+++ statics.java
@@ -3,23 +3,36 @@ class Mobile {
     int price;
     static String name; // it makes the member of Class not of object
 
+    static {
+        name = "phone";
+        System.out.println("in static block");
+    }
+
+    public Mobile() {
+        brand = "";
+        price = 199;
+        System.out.println("in constructor block");
+    }
+
     public void show() {
         System.out.println(brand + " : " + price + " : " + name);
     }
 }
 
 public class statics {
-    public static void main(String a[]) {
+    public static void main(String a[]) throws ClassNotFoundException {
 
-        Mobile m1 = new Mobile();
-        m1.brand = "oppo";
-        m1.price = 299;
-        Mobile.name = "phone";
-        m1.show();
+        Class.forName("Mobile");
 
-        Mobile m2 = new Mobile();
-        m2.brand = "Vivo";
-        m2.price = 249;
-        m2.show();
+//        Mobile m1 = new Mobile();
+//        m1.brand = "oppo";
+//        m1.price = 299;
+//        Mobile.name = "phone";
+//        m1.show();
+//
+//        Mobile m2 = new Mobile();
+//        m2.brand = "Vivo";
+//        m2.price = 249;
+//        m2.show();
     }
 }
