24b120a05d7e8016148eebbdccca90f8c9b8de63
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-08 23:06:57 +0530

    naming convention

---
+++ naming_convention.java
@@ -0,0 +1,15 @@
+public class naming_convention {
+    public static void main(String[] args) {
+        System.out.println("Naming convention in Java");
+    }
+}
+
+/*
+ *  camel casing
+ *  class and interfaces name should be capitalized - Human.
+ * variables and methods should be in lower case  -marks,name,age,get(),
+ * constants should be in upper case - PIE,BRAND.
+ * names with two letters - first should be smaller case and another must be in capitalized.  - camelcasing - getValues(),employeeAge.
+ * snake casing - separate the words by underscore(_) - number_of_employees
+ * a class name with two words should be capitalized to both - NewStudent
+ * */
