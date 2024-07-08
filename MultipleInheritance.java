92c70ebb4fa37c71cb861212d5260b0bca6d6a95
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-09 00:15:22 +0530

    multiple inheritance

---
+++ MultipleInheritance.java
@@ -0,0 +1,22 @@
+class L {
+
+}
+
+class M {
+
+}
+
+class N extends L {
+}
+
+
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+}
+/*
+ * multiple inheritance in Java doesn't work due to ambiguity(confusion) as same method may exist to more than one parent
+ * that's why JAVA removed this feature.
+ * */
