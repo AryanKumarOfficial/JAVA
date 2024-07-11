8fcea540ee5a098074d9b153bb89a265d6515110
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-11 12:44:25 +0530

    inheritance in interface

--- interface_extra/Demo.java
+++ interface_extra/Demo.java
@@ -1,18 +1,33 @@
 package interface_extra;
 
 interface A {
-    int n = 10; //final and static
-    String name = "asdf";
 
     void show();
 
     void config();
 }
 
-class B implements A {
+interface X {
+    void run();
+}
+
+interface Y extends A, X {
+    void display();
+}
+
+class B implements A, Y {
+    @Override
+    public void display() {
+        System.out.println("displaying...");
+    }
+
     @Override
     public void show() {
         System.out.println("in show");
+    }
+
+    public void run() {
+        System.out.println("running...");
     }
 
     @Override
@@ -23,12 +38,11 @@ class B implements A {
 
 public class Demo {
     public static void main(String[] args) {
-        A obj;
+        Y obj;
         obj = new B();
         obj.show();
         obj.config();
-        System.out.println(A.n);
-        System.out.println(A.name);
+        obj.run();
     }
 }
 
@@ -37,5 +51,12 @@ public class Demo {
  * methods in interface are by default public and abstract
  * we can't create object of interface, but we can create reference of interface
  * we can't create object of interface, but we can create object of class which implements interface.
+ *
+ * interface can extend multiple interfaces
+ * class can implement multiple interfaces
+ * when a class implements multiple interfaces, it has to provide implementation of all methods of all interfaces
+ * to extend an interface, use extends keyword
+ * to implement an interface, use implements keyword
+ * interface can extend multiple interfaces
  *
  * */
