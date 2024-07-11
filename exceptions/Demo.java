051a1c374cc5fe25d51754ba2790766afcdadb3b
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-11 20:21:20 +0530

    exception handling using try catch block

--- exceptions/Demo.java
+++ exceptions/Demo.java
@@ -2,12 +2,21 @@ package exceptions;
 
 public class Demo {
     public static void main(String[] args) {
-        int i = 0;
+        int i = 1;
         int j = 0;
+        int[] num = new int[6];
+        String str = null;
         try {
             j = 18 / i;
+            System.out.println(num[0]);
+            System.out.println(num[5]);
+            System.out.println(str.length());
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Array index out of bounds");
         } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
+            System.out.println("something went wrong");
         }
         System.out.println(j);
         System.out.println("bye");
