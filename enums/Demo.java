b9c315b50ef38e426768dcb6d6b8ad63dd686c71
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-11 13:13:10 +0530

    enums with conditional statements

--- enums/Demo.java
+++ enums/Demo.java
@@ -1,11 +1,13 @@
 package enums;
 
 enum Status {
-    Running, Failed, Pending, Success;
+    Running, Failed, Pending, Success
 }
 
 public class Demo {
     public static void main(String[] args) {
+       /*
+
         Status status = Status.Success;
         Status[] statuses = Status.values();
         System.out.println(status.ordinal()); // 3
@@ -14,10 +16,42 @@ public class Demo {
         for (Status s : statuses) {
             System.out.println(s + " : " + s.ordinal());
         }
+       * */
+
+        Status s = Status.Failed;
+        if (s == Status.Running) System.out.println("all good");
+        else if (s == Status.Failed) System.out.println("Try again");
+        else if (s == Status.Pending) System.out.println("Please wait");
+        else System.out.println("Done");
+
+        switch (s) {
+            case Running:
+                System.out.println("all good");
+                break;
+            case Failed:
+                System.out.println("Try again");
+                break;
+            case Pending:
+                System.out.println("Please wait");
+                break;
+            case Success:
+                System.out.println("Done");
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + s);
+        }
     }
 }
 
 /*
-* enums are used to define a group of named constants.
-*
-* */
+ * enums are used to define a group of named constants.
+ * it is a special "class" that represents a group of constants (unchangeable variables, like final variables).
+ * To create an enum, use the enum keyword (instead of class or interface), and separate the constants with a comma.
+ * The enum constants have initial value that starts from 0, 1, 2, 3, and so on.
+ * You can access enum constants with the dot syntax.
+ * You can loop through the enum constants with the values() method.
+ * You can compare enum constants with the == operator.
+ * You can use the switch statement to select enum constants.
+ * You can add fields, constructors, and methods to an enum.
+ *
+ * */
