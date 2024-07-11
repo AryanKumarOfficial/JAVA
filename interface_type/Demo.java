6f12e27d5147f473620058eb88e09048235f9a08
Aryan Kumar <Aryanak9163@gmail.com>
2024-07-11 19:22:42 +0530

    types of interfaces

---
+++ interface_type/Demo.java
@@ -0,0 +1,19 @@
+package interface_type;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
+
+/*
+ * type of interface:
+ * 1. Normal Interface
+ * 2. Single Abstract Method (SAM) Interface / Functional Interface
+ * 3. Marker Interface
+ *
+ *  1. Normal Interface: An interface that has more than one abstract method.
+ * 2. Single Abstract Method (SAM) Interface / Functional Interface: An interface that has only one abstract method.
+ * 3. Marker Interface: An interface that does not have any abstract method. It is used to inform the compiler that the class implementing the interface has some special behavior.
+ *
+ * */
